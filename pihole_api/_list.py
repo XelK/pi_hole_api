ee0896b40a4765eae02354b956243e8683342d09
xelk <haveiteasy@gmail.com>
2021-11-01 14:31:11 +0100

    update workflow, update api

--- pihole_api/_list.py
+++ pihole_api/_list.py
@@ -3,25 +3,25 @@ Pihole python API client.
 Module for black/white list management
 """
 
-types = ("white", "black")
-err = "Not correct type, use: " + str(types)
+LIST_TYPES = ("white", "black")
+ERR_MSG_MSG = "Not correct type, use: " + str(LIST_TYPES)
 
 
 def get_domains(pihole, type) -> dict:
-    if type not in types:
-        return err
+    if type not in LIST_TYPES:
+        return ERR_MSG_MSG
     _list_url = pihole.url + "scripts/pi-hole/php/groups.php"
     _params = {"action": "get_domains", "showtype": type, "token": pihole.token}
     return pihole.session.post(_list_url, _params).json()
 
 
 def add_domain(pihole, type, domain, comment) -> dict:
-    if type not in types:
-        return err
+    if type not in LIST_TYPES:
+        return ERR_MSG
     _list_url = pihole.url + "scripts/pi-hole/php/groups.php"
     _params = {
         "action": "add_domain",
-        "type": types.index(type),
+        "type": LIST_TYPES.index(type),
         "domain": domain,
         "comment": comment,
         "token": pihole.token,
@@ -30,12 +30,12 @@ def add_domain(pihole, type, domain, comment) -> dict:
 
 
 def replace_domain(pihole, type, domain, comment) -> dict:
-    if type not in types:
-        return err
+    if type not in LIST_TYPES:
+        return ERR_MSG
     _list_url = pihole.url + "scripts/pi-hole/php/groups.php"
     _params = {
         "action": "replace_domain",
-        "type": types.index(type),
+        "type": LIST_TYPES.index(type),
         "domain": domain,
         "comment": comment,
         "token": pihole.token,
@@ -44,12 +44,12 @@ def replace_domain(pihole, type, domain, comment) -> dict:
 
 
 def delete_domain(pihole, type, domain, comment) -> dict:
-    if type not in types:
-        return err
+    if type not in LIST_TYPES:
+        return ERR_MSG
     _list_url = pihole.url + "scripts/pi-hole/php/groups.php"
     _params = {
         "action": "delete_domain",
-        "type": types.index(type),
+        "type": LIST_TYPES.index(type),
         "domain": domain,
         "comment": comment,
         "token": pihole.token,
