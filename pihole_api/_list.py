e34aa69e554bae9443d8701daeee6ff4854423c6
xelk <haveiteasy@gmail.com>
2021-11-01 14:40:57 +0100

    fix var name error

--- pihole_api/_list.py
+++ pihole_api/_list.py
@@ -4,7 +4,7 @@ Module for black/white list management
 """
 
 LIST_TYPES = ("white", "black")
-ERR_MSG_MSG = "Not correct type, use: " + str(LIST_TYPES)
+ERR_MSG = "Not correct type, use: " + str(LIST_TYPES)
 
 
 def get_domains(pihole, showtype) -> dict:
@@ -12,7 +12,7 @@ def get_domains(pihole, showtype) -> dict:
     Return list of domains from white/black list
     """
     if showtype not in LIST_TYPES:
-        return ERR_MSG_MSG
+        return ERR_MSG
     _list_url = pihole.url + "scripts/pi-hole/php/groups.php"
     _params = {"action": "get_domains", "showtype": showtype, "token": pihole.token}
     return pihole.session.post(_list_url, _params).json()
